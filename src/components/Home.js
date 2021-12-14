7a2fc2a52b00707463b395670994bad39081fa00
Emrikcah <curryandr3@gmail.com>
2021-12-14 09:58:00 -0500

    initial setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
-export default function Home() {
+
+export function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -78,5 +79,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
